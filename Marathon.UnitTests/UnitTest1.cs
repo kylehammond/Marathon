5cf701177fe6867874720cb9df2959ac263ce4db
Kyle Hammond <kyle.hammond@gmail.com>
2022-01-29 11:05:28 -0600

    Added UnitTests project

---
+++ Marathon.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Marathon.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
