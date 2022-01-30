bca97a09201ada004e998525ba722ed7a80e9d84
Kyle Hammond <kyle.hammond@gmail.com>
2022-01-30 08:30:41 -0600

    Adding hosting and DI packages

--- Marathon.HangFire/Program.cs
+++ Marathon.HangFire/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 // Build Configuration
 var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+
+using IHost host = 
 
 var configurationRoot = new ConfigurationBuilder()
     .AddJsonFile($"appsettings.json", true, true)
