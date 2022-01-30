4c47937c219c88665574f80887fdf52d4e4113d2
Kyle Hammond <kyle.hammond@gmail.com>
2022-01-30 09:08:50 -0600

    Adding serilog

--- Marathon.HangFire/Program.cs
+++ Marathon.HangFire/Program.cs
@@ -5,14 +5,10 @@ using Microsoft.Extensions.Hosting;
 // Build Configuration
 var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
 
-using IHost host = 
-
 var configurationRoot = new ConfigurationBuilder()
     .AddJsonFile($"appsettings.json", true, true)
     .AddJsonFile($"appsettings.{environment}.json", true, true)
     .AddEnvironmentVariables()
     .Build();
 
-var appSettings = configurationRoot.GetSection(nameof(AppSettings)).Get<AppSettings>();
 
-Console.WriteLine($"Current Environment:  {appSettings.Environment}");
